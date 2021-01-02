ece26b755bf3ee2f864c0fb6b045430f0e9d12e0
Zeydi <oussema.zeidi@ieee.org>
2021-01-02 14:36:05 +0100

    added longestWord script

---
+++ FreeCodeCamp/longestWord.js
@@ -0,0 +1,10 @@
+function findLongestWordLength(str) {
+  let strArr = str.split(' ');
+  return strArr.reduce(function (elt, nextElt) {
+    return elt.length > nextElt.length ? elt : nextElt;
+  }).length;
+}
+
+console.log(
+  findLongestWordLength('The quick brown fox jumped over the lazy dog')
+);
